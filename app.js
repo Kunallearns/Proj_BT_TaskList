b43594c7f8bec5b0deb65c1a37f4a1886f4feba6
Kunal-ThinkPad <kunallifelearner@gmail.com>
2020-06-07 02:10:21 +0800

    File updated

--- app.js
+++ app.js
@@ -7,14 +7,28 @@ const clearBtn = document.querySelector(".clear-tasks")
 
 //  function to load all event listners
 
+
 loadEventListeners();
+
 
 /* define LoadEventListener function */
 function loadEventListeners() {
     //  Add Task event
     form.addEventListener("submit", addTask)
+    //  Remove task event  - each specific task by clicking on x icon
+    taskList.addEventListener("click", removeTask)
+
+    //Remove the entire list by clicking on clear Tasks Button
+
+    clearBtn.addEventListener("click", clearTasks)
+
+    //Filter the Task events 
+    filter.addEventListener("keyup", filterTasks)
 }
-//  Add Task
+
+
+
+/*  addTask: Add Task */
 function addTask(e) {
     if (taskInput.value === "") {
         alert("add a task")
@@ -38,4 +52,58 @@ function addTask(e) {
     taskInput.value = "" /* Clear Input */
 
     e.preventDefault()
+}
+
+
+
+/* removeTask: remove task  Since they are multiple items and can be dynamic in nature if.e. they can be added as per user discretion, we'll havr to rely on event delegation and we need to use the event listener on parent UL with the class "taskList" itself  */
+function removeTask(e) {
+    console.log(e)
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if (confirm("Are you sure ?")) {
+            e.target.parentElement.parentElement.remove() /* parent of the parent is the LI item which needs to be wiped off when the click happens */
+
+        }
+    }
+}
+
+
+
+/* clearBtn: Clear tasks button */
+function clearTasks(e) {
+    console.log(e)
+    if (e.target.classList.contains("clear-tasks")) {
+        if (confirm("Are you sure?")) {
+            // taskList.innerHTML = "" /* can do this or below - both work */
+
+            //  but the wjile loop is faster: per the defination below, the loop would continue to work till there is s a first child of the taskList
+            while (taskList.firstChild) {
+
+                taskList.removeChild(taskList.firstChild)
+            }
+
+            //     console.log(e)
+        };
+    }
+}
+
+
+function filterTasks(e) {
+    // console.log(e)
+    const text = e.target.value.toLowerCase();
+
+
+
+
+    /*In below, you can use forEach because querySelectorAll returns a NodeList and not HTML collection and forEach() can be used on NodeList. The same would NOT be true for getElementsByClass or tag because that wouldve returned HTML collection and then you wouldve had to convert it to array first in order to apply forEach() */
+    document.querySelectorAll(".collection-item").forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = "block"
+
+        } else {
+            task.style.display = "none"
+        }
+    })
+
 }
